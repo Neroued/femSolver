493e1f214a7609c38f87c7099b17dd27bd98dd6b
neroued <neroued@gmail.com>
2024-11-07 22:51:33 +0100

    添加const标识符

--- include/Mesh.h
+++ include/Mesh.h
@@ -9,6 +9,6 @@ public:
     TArray<Vec3> vertices;        // 存储(x,y,z)点的坐标
     TArray<TVec3<int>> triangles; // 用(0,1,2)表示一个三角形
 
-    size_t vertex_count() { return vertices.size; }
-    size_t triangle_count() { return triangles.size; }
+    size_t vertex_count() const { return vertices.size; }
+    size_t triangle_count() const { return triangles.size; }
 };
