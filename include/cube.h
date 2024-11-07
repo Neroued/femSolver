c5a7198c06f222cf105f9df4fac83211649f062e
neroued <neroued@gmail.com>
2024-11-07 22:52:27 +0100

    优化算法，使用int64哈希表，效率极佳

--- include/cube.h
+++ include/cube.h
@@ -4,6 +4,8 @@
 #include <vector>
 #include <timer.h>
 #include <iostream>
+#include <unordered_map>
+
 
 /* 生成立方体网格, 中心为原点，边长为2
  * 对于有n个子分割的网格
@@ -11,7 +13,6 @@
  * 1. 生成顶点，同时使用hash表记录每个顶点的对应下标，生成一个不包含重复点的顶点集合 vertex_index_map
  * 2. 同时生成一个有重复点时下标与无重复点时下标的hash表 dupToNoDupIndex
  * 3. 根据dupToNoDupIndex创建三角形
- *
  */
 
 int load_cube(Mesh &m, const int subdiv)
@@ -21,49 +22,87 @@ int load_cube(Mesh &m, const int subdiv)
 
     int n = subdiv + 1;
 
-    Vec3 Axis_x = {1, 0, 0};
-    Vec3 Axis_y = {0, 1, 0};
-    Vec3 Axis_z = {0, 0, 1};
+    struct Face
+    {
+        int axis; // 0:x, 1:y, 2:z
+        int dir;  // 0:负方向, 1:正方向
+    };
 
-    std::vector<std::vector<Vec3>> r = {{Axis_z, Axis_x, Axis_y}, {Axis_x, Axis_y, Axis_z}, {Axis_y, Axis_x, Axis_z}};
-    std::unordered_map<Vec3, int> vertex_index_map;
-    vertex_index_map.reserve(6 * subdiv * subdiv + 2); // 为哈希表预分配空间, 数量为不重复点的数量gdb
-    int *dupToNoDupIndex = new int[6 * n * n];
-    m.vertices.resize(6 * subdiv * subdiv + 2); // 总共6(subdiv+1)^2个点，实际不重复的点有6*subdiv^2+2个
+    std::vector<Face> faces = {{0, 0},
+                               {0, 1},
+                               {1, 0},
+                               {1, 1},
+                               {2, 0},
+                               {2, 1}};
+
+    int totalVertices = 6 * n * n;                // 重复顶点有6n^2个
+    int uniqueVertices = 6 * subdiv * subdiv + 2; // 不重复的顶点有6 * subdiv^2 + 2个
+
+    // 使用一个hash函数将Vec3映射到int64, 避免使用Vec3作为主键
+    std::unordered_map<int64_t, int> vertex_index_map;
+    vertex_index_map.reserve(uniqueVertices);
+
+    // 从重复点索引到不重复点索引的map
+    int *dupToNoDupIndex = new int[totalVertices];
+    m.vertices.resize(totalVertices);
 
     int t = 0; // t 表示存在重复点的下标
     int p = 0; // p 表示不重复点的下标
-    for (auto v : r)
+
+    float invSubdiv = 1.0f / subdiv; // 提前计算好减少浮点数除法
+
+    Vec3 z{-1, -1, -1};
+    for (const Face &face : faces)
     {
-        Vec3 a = v[0];
-        Vec3 b = v[1];
-        Vec3 c = v[2];
-        for (float f = 0.f; f < 1.1f; f += 1)
+        int axis = face.axis;
+        int dir = face.dir;
+
+        int idx1 = (axis + 1) % 3;
+        int idx2 = (axis + 2) % 3;
+
+        for (int i = 0; i < n; ++i)
         {
-            for (int i = 0; i < n; ++i)
+            for (int j = 0; j < n; ++j)
             {
-                for (int j = 0; j < n; ++j)
+                /* 先使用整数坐标表示一个点，方便后续使用hash
+                 * 例如face = {0, 1}, 即表示垂直于x轴，位于正半轴区域的面
+                 * axis = 0, dir = 1, idx1 = 1, idx2 = 2
+                 * 经过处理后coords存储的是 [subdiv, i, j] 这样的点
+                 * 之后经过处理可以变成需要的范围
+                 */
+                int coords[3] = {0, 0, 0};
+                coords[axis] = dir * subdiv;
+
+                coords[idx1] = i;
+                coords[idx2] = j;
+
+                /* Hash函数的部分
+                 * 将三个坐标映射到int64的范围中作为主键
+                 * 需要subdiv < 2^20
+                 */
+                int64_t key = (int64_t)coords[0] | (int64_t)coords[1] << 20 | (int64_t)coords[2] << 40;
+
+                auto it = vertex_index_map.find(key);
+                if (it == vertex_index_map.end()) // 若这个点是首次出现
                 {
-                    Vec3 tmp;
-                    Vec3 z {-1,-1,-1};
-                    tmp = (a * f + b * (float(i) / subdiv) + c * (float(j) / subdiv)) * 2.f + z;
+                    vertex_index_map[key] = p; // 存储这个点的位置为p
+                    dupToNoDupIndex[t] = p;
 
-                    auto it = vertex_index_map.find(tmp);
-                    if (it == vertex_index_map.end()) // 若这个点是首次出现
-                    {
-                        
-                        vertex_index_map[tmp] = p; // 存储这个点的位置为p
-                        m.vertices[p] = tmp;
-                        dupToNoDupIndex[t] = p;
-                        ++p;
-                    }
-                    else
-                    {
-                        dupToNoDupIndex[t] = it->second; // 将重复点的下标对应到不重复点的下标
-                    }
+                    // 计算顶点的位置
+                    float fx = coords[0] * invSubdiv * 2.0f - 1.0f;
+                    float fy = coords[1] * invSubdiv * 2.0f - 1.0f;
+                    float fz = coords[2] * invSubdiv * 2.0f - 1.0f;
 
-                    ++t;
+                    m.vertices[p] = {fx, fy, fz};
+
+                    ++p;
                 }
+                else
+                {
+                    dupToNoDupIndex[t] = it->second; // 将重复点的下标对应到不重复点的下标
+                }
+
+                ++t;
             }
         }
     }
@@ -76,20 +115,29 @@ int load_cube(Mesh &m, const int subdiv)
     m.triangles.resize(12 * subdiv * subdiv); // 共计12n^2个三角形
 
     t = 0;
-    for (int i = 0; i < 6; ++i)
+    int faceVertexOffset = 0;
+    for (int faceIdx = 0; faceIdx < 6; ++faceIdx)
     {
-        int n = i * (subdiv + 1) * (subdiv + 1);
-        for (int j = 0; j < subdiv; ++j)
+        for (int i = 0; i < subdiv; ++i)
         {
-            int p = n + j * (subdiv + 1);
-            for (int k = 0; k < subdiv; ++k)
+            for (int j = 0; j < subdiv; ++j)
             {
-                m.triangles[t++] = {dupToNoDupIndex[p + k], dupToNoDupIndex[p + k + 1], dupToNoDupIndex[p + k + subdiv + 1]};
-                m.triangles[t++] = {dupToNoDupIndex[p + k + 1],
-                                    dupToNoDupIndex[p + k + subdiv + 2],
-                                    dupToNoDupIndex[p + k + subdiv + 1]};
+                int idx0 = faceVertexOffset + i * n + j;           // 左下的点
+                int idx1 = faceVertexOffset + i * n + j + 1;       // 右下
+                int idx2 = faceVertexOffset + (i + 1) * n + j;     // 左上
+                int idx3 = faceVertexOffset + (i + 1) * n + j + 1; // 右上
+
+                int v0 = dupToNoDupIndex[idx0];
+                int v1 = dupToNoDupIndex[idx1];
+                int v2 = dupToNoDupIndex[idx2];
+                int v3 = dupToNoDupIndex[idx3];
+
+                m.triangles[t++] = {v0, v1, v2};
+                m.triangles[t++] = {v1, v2, v3};
             }
         }
+
+        faceVertexOffset += n * n;
     }
 
     delete[] dupToNoDupIndex;
