60788ba4d9c3af610cb3cc0d06545ee8963becce
neroued <neroued@gmail.com>
2024-11-12 03:09:13 +0100

    test

--- src/test_glfw.cpp
+++ src/test_glfw.cpp
@@ -1,260 +1,181 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <vector>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
-#include "Mesh.h"
-#include "cube.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
 
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-std::string loadShaderSource(const char* filepath);
-unsigned int createShaderProgram(const char* vertexPath, const char* fragmentPath);
-void setupMesh(Mesh& mesh, int subdiv, unsigned int& VAO, unsigned int& VBO, unsigned int& EBO);
-void renderMesh(unsigned int VAO, unsigned int shaderProgram);
-void testMeshRendering();
-
-int main()
-{
-    testMeshRendering();
-    return 0;
-}
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
 }
 
-std::string loadShaderSource(const char* filepath)
-{
+std::string readShaderSource(const char* filepath) {
     std::ifstream file(filepath);
-    if (!file.is_open())
-    {
-        std::cerr << "Failed to open shader file: " << filepath << std::endl;
-        return "";
-    }
     std::stringstream buffer;
     buffer << file.rdbuf();
     return buffer.str();
 }
 
-unsigned int createShaderProgram(const char* vertexPath, const char* fragmentPath)
-{
-    std::string vertexCode = loadShaderSource(vertexPath);
-    std::string fragmentCode = loadShaderSource(fragmentPath);
+unsigned int compileShader(unsigned int type, const char* source) {
+    unsigned int shader = glCreateShader(type);
+    glShaderSource(shader, 1, &source, nullptr);
+    glCompileShader(shader);
 
-    if (vertexCode.empty() || fragmentCode.empty())
-    {
-        std::cerr << "Shader source code is empty" << std::endl;
-        return 0;
-    }
-
-    const char* vertexShaderSource = vertexCode.c_str();
-    const char* fragmentShaderSource = fragmentCode.c_str();
-
-    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
     int success;
-    char infoLog[512];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        std::cerr << "Vertex shader compilation failed\n" << infoLog << std::endl;
-        return 0;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        char infoLog[512];
+        glGetShaderInfoLog(shader, 512, nullptr, infoLog);
+        std::cerr << "ERROR::SHADER::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    return shader;
+}
+
+int main() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "3D Tetrahedron with Colored Edges", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cerr << "Failed to initialize GLAD" << std::endl;
+        return -1;
     }
 
-    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-        std::cerr << "Fragment shader compilation failed\n" << infoLog << std::endl;
-        return 0;
-    }
+    // 设置白色背景
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+    glEnable(GL_DEPTH_TEST);
+
+    // 顶点数据
+    float vertices[] = {
+        -0.5f, -0.5f, 0.2f,  1.0f, 0.0f, 0.0f,  // 第一个点，红色
+         0.5f, -0.5f, 0.8f,  0.0f, 1.0f, 0.0f,  // 第二个点，绿色
+         0.0f,  0.5f, 0.5f,  0.0f, 0.0f, 1.0f,  // 第三个点，蓝色
+         0.0f, -0.5f, -0.5f,  1.0f, 1.0f, 0.0f  // 第四个点，黄色
+    };
+
+    // 索引数组
+    unsigned int indices_faces[] = {
+        0, 1, 2,  // 面 1
+        0, 1, 3,  // 面 2
+        0, 2, 3,  // 面 3
+        1, 2, 3   // 面 4
+    };
+
+    unsigned int indices_edges[] = {
+        0, 1,  // 边 1
+        0, 2,  // 边 2
+        0, 3,  // 边 3
+        1, 2,  // 边 4
+        1, 3,  // 边 5
+        2, 3   // 边 6
+    };
+
+    unsigned int VBO, VAO_faces, EBO_faces, VAO_edges, EBO_edges;
+    glGenVertexArrays(1, &VAO_faces);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO_faces);
+
+    // 设置面数据
+    glBindVertexArray(VAO_faces);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO_faces);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices_faces), indices_faces, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    // 设置边数据
+    glGenVertexArrays(1, &VAO_edges);
+    glGenBuffers(1, &EBO_edges);
+
+    glBindVertexArray(VAO_edges);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO_edges);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices_edges), indices_edges, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    std::string vertexCode = readShaderSource("shader/vertex_shader.glsl");
+    std::string fragmentCode = readShaderSource("shader/fragment_shader.glsl");
+
+    unsigned int vertexShader = compileShader(GL_VERTEX_SHADER, vertexCode.c_str());
+    unsigned int fragmentShader = compileShader(GL_FRAGMENT_SHADER, fragmentCode.c_str());
 
     unsigned int shaderProgram = glCreateProgram();
     glAttachShader(shaderProgram, vertexShader);
     glAttachShader(shaderProgram, fragmentShader);
     glLinkProgram(shaderProgram);
+
+    int success;
     glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success)
-    {
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cerr << "Shader program linking failed\n" << infoLog << std::endl;
-        return 0;
+    if (!success) {
+        char infoLog[512];
+        glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
+        std::cerr << "ERROR::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
     }
 
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
-    return shaderProgram;
-}
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glUseProgram(shaderProgram);
 
-void setupMesh(Mesh& mesh, int subdiv, unsigned int& VAO, unsigned int& VBO, unsigned int& EBO)
-{
-    load_cube(mesh, subdiv);
+        glm::mat4 model = glm::rotate(glm::mat4(1.0f), (float)glfwGetTime(), glm::vec3(1.0f, 1.0f, 1.0f));
+        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -3.0f));
+        glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
 
-    std::vector<float> vertices;
-    for (const auto& vertex : mesh.vertices)
-    {
-        vertices.push_back(vertex.x);
-        vertices.push_back(vertex.y);
-        vertices.push_back(vertex.z);
-        
-        // Adding color attribute, for example, orange
-        vertices.push_back(1.0f); // R
-        vertices.push_back(0.5f); // G
-        vertices.push_back(0.0f); // B
-    }
-
-    std::vector<unsigned int> indices;
-    for (const auto& triangle : mesh.triangles)
-    {
-        indices.push_back(triangle.x);
-        indices.push_back(triangle.y);
-        indices.push_back(triangle.z);
-    }
-
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
-
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), indices.data(), GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-    
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-}
-
-void renderMesh(unsigned int VAO, unsigned int shaderProgram)
-{
-    glUseProgram(shaderProgram);
-
-    glm::mat4 model = glm::mat4(1.0f);
-    glm::mat4 view = glm::lookAt(glm::vec3(0.0f, 2.0f, -10.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-    glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-
-    unsigned int modelLoc = glGetUniformLocation(shaderProgram, "model");
-    unsigned int viewLoc = glGetUniformLocation(shaderProgram, "view");
-    unsigned int projLoc = glGetUniformLocation(shaderProgram, "projection");
-
-    if (modelLoc != -1)
+        int modelLoc = glGetUniformLocation(shaderProgram, "model");
         glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-    if (viewLoc != -1)
+
+        int viewLoc = glGetUniformLocation(shaderProgram, "view");
         glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-    if (projLoc != -1)
+
+        int projLoc = glGetUniformLocation(shaderProgram, "projection");
         glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
-    glBindVertexArray(VAO);
-    glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+        // 绘制四面体表面
+        glUniform1i(glGetUniformLocation(shaderProgram, "isEdge"), GL_FALSE);
+        glBindVertexArray(VAO_faces);
+        glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
 
-    // Draw edges in wireframe mode
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    glLineWidth(2.0f); // Set line width to make edges more visible
-    glUniform4f(glGetUniformLocation(shaderProgram, "color"), 0.0f, 0.0f, 0.0f, 1.0f); // Set line color to black
-    glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
-    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+        // 绘制四面体边
+        glUniform1i(glGetUniformLocation(shaderProgram, "isEdge"), GL_TRUE);
+        glBindVertexArray(VAO_edges);
+        glDrawElements(GL_LINES, 12, GL_UNSIGNED_INT, 0);
 
-    // Draw edges in wireframe mode
-    glUseProgram(shaderProgram);
-    glUniform4f(glGetUniformLocation(shaderProgram, "color"), 0.0f, 0.0f, 0.0f, 1.0f); // Set line color to black
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
-    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-
-    // Draw edges in wireframe mode
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
-    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-}
-
-void testMeshRendering()
-{
-    // Initialize GLFW
-    if (!glfwInit())
-    {
-        std::cerr << "GLFW initialization failed" << std::endl;
-        return;
-    }
-
-    // Set GLFW context version and profile
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-    // Create window
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Test Mesh Rendering", nullptr, nullptr);
-    if (window == nullptr)
-    {
-        std::cerr << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return;
-    }
-    glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-    // Load GLAD
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cerr << "Failed to initialize GLAD" << std::endl;
-        return;
-    }
-
-    // Enable depth test
-    glEnable(GL_DEPTH_TEST);
-
-    // Create shader program
-    unsigned int shaderProgram = createShaderProgram("./shader/vertex_shader.glsl", "./shader/fragment_shader.glsl");
-    if (shaderProgram == 0)
-    {
-        glfwTerminate();
-        return;
-    }
-
-    // Set up mesh data
-    int subdiv = 1; // Use a simple cube with minimal subdivision for testing
-    Mesh mesh;
-    unsigned int VAO, VBO, EBO;
-    setupMesh(mesh, subdiv, VAO, VBO, EBO);
-
-    // Render loop
-    while (!glfwWindowShouldClose(window))
-    {
-        // Clear buffers
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        // Render mesh
-        renderMesh(VAO, shaderProgram);
-
-        // Swap buffers and poll events
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    // Cleanup
-    glDeleteVertexArrays(1, &VAO);
+    glDeleteVertexArrays(1, &VAO_faces);
+    glDeleteVertexArrays(1, &VAO_edges);
     glDeleteBuffers(1, &VBO);
-    glDeleteBuffers(1, &EBO);
+    glDeleteBuffers(1, &EBO_faces);
+    glDeleteBuffers(1, &EBO_edges);
     glDeleteProgram(shaderProgram);
+
     glfwDestroyWindow(window);
     glfwTerminate();
+    return 0;
 }
