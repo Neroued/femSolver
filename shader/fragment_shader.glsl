a5c9377f83c4a1bb7d07be01cfa3bdbecd4d1660
neroued <neroued@gmail.com>
2024-11-15 00:29:15 +0100

    更新shader适应FEMData渲染

--- shader/fragment_shader.glsl
+++ shader/fragment_shader.glsl
@@ -1,11 +1,15 @@
 #version 330 core
+in vec3 vertexColor; // 从顶点着色器传入的颜色
 out vec4 FragColor;
 
 uniform bool isEdgeMode;  // true: 绘制边，false: 绘制面
+uniform bool isFEMData;   // true: 绘制FEMData, false: 绘制Mesh
 
 void main() {
     if (isEdgeMode) {
-        FragColor = vec4(0.0, 0.0, 0.0, 1.0);  // 边
+        FragColor = vec4(0.0, 0.0, 0.0, 1.0); // 边：黑色
+    } else if (isFEMData) {
+        FragColor = vec4(vertexColor, 1.0);   // 面：使用顶点颜色
     } else {
         FragColor = vec4(1.0, 0.5 , 1.0, 1.0);  // 面
     }
--- shader/vertex_shader.glsl
+++ shader/vertex_shader.glsl
@@ -1,10 +1,14 @@
 #version 330 core
-layout (location = 0) in vec3 aPos;
+layout (location = 0) in vec3 aPos;    // 顶点位置
+layout (location = 1) in vec3 aColor; // 顶点颜色
+
+out vec3 vertexColor; // 将颜色传递给片段着色器
 
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
 void main() {
-    gl_Position = projection * view * model * vec4(aPos, 1.0);
+    gl_Position = projection * view * model * vec4(aPos, 1.0); // 应用变换矩阵
+    vertexColor = aColor; // 传递顶点颜色到片段着色器
 }
